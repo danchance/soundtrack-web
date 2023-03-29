4c9eaf420dd2c06122d38b525e35bbb23ea766d8
Daniel Chance <dan.chance@outlook.com>
2023-03-29 20:10:51 +0100

    add graph view for top items

--- src/components/top_items.tsx
+++ src/components/top_items.tsx
@@ -1,10 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '@/styles/components/top_items.module.sass';
 import Image from 'next/image';
 import Link from 'next/link';
 import GridSVG from '@/assets/icons/grid.svg';
 import ListSVG from '@/assets/icons/list.svg';
 import GraphSVG from '@/assets/icons/graph.svg';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, {
+  id: 'addImageLabels',
+  afterDatasetsDraw: (chart: any) => {
+    let imgs = chart.config.options.plugins.addImageLabels;
+    var ctx = chart.ctx;
+    var xAxis = chart.scales['x'];
+    var yAxis = chart.scales['y'];
+    const width = (xAxis.width / 10) * 0.75;
+    xAxis.ticks.forEach((value: any, index: number) => {
+      var x = xAxis.getPixelForTick(index);
+      ctx.drawImage(
+        imgs[index],
+        x - width / 2,
+        yAxis.bottom + 10,
+        width,
+        width
+      );
+    });
+  }
+});
 
 type TopItemsProps = {
   itemList: Array<Item>;
@@ -39,9 +68,65 @@ enum View {
  * @returns
  */
 const TopItems = ({ itemList, itemType }: TopItemsProps) => {
-  const [view, setView] = useState<View>(View.GRID);
+  const [view, setView] = useState<View>(View.GRAPH);
   // Set correct heading based on item type we are displaying.
   const heading = `Top ${itemType}s`;
+
+  const images = itemList.map((item) => {
+    const img = document.createElement('img');
+    img.src = item.artwork;
+    return img;
+  });
+
+  const options = {
+    responsive: true,
+    plugins: {
+      addImageLabels: images,
+      legend: {
+        display: false
+      },
+      tooltip: {
+        callbacks: {
+          title: (tooltipItem: any) => {
+            const index = tooltipItem[0].dataIndex;
+            if (itemType === TopItemTypes.TRACK) {
+              return itemList[index].trackName;
+            }
+            if (itemType === TopItemTypes.ALBUM) {
+              return itemList[index].albumName;
+            }
+            if (itemType === TopItemTypes.ARTIST) {
+              return itemList[index].artistName;
+            }
+          }
+        }
+      }
+    },
+    scales: {
+      y: {
+        ticks: {
+          beginAtZero: true
+        }
+      },
+      x: {
+        ticks: {
+          padding: 30
+        }
+      }
+    }
+  };
+
+  const data = {
+    labels: Array.from({ length: 10 }).map((_) => ''),
+    datasets: [
+      {
+        label: 'Streams',
+        data: itemList.map((item) => item.count),
+        fill: false,
+        backgroundColor: 'rgba(255, 99, 132, 0.5)'
+      }
+    ]
+  };
 
   return (
     <div className={styles['top-items']}>
@@ -107,6 +192,11 @@ const TopItems = ({ itemList, itemType }: TopItemsProps) => {
               </div>
             </div>
           ))}
+        </div>
+      )}
+      {view === View.GRAPH && (
+        <div className={styles['graph']}>
+          <Bar options={options as any} data={data} />
         </div>
       )}
       <div className={styles['footer']}>
