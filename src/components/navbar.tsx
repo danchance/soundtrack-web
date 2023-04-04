67760f65aa1b7e927edb4514e7d6aba2edb49e1d
Daniel Chance <dan.chance@outlook.com>
2023-04-04 20:10:09 +0100

    add search bar and profile image styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,29 +10,32 @@ const NavBar = ({}) => {
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {};
 
   return (
-    <div className={styles['container']}>
-      <div className={styles['navbar']}>
-        <div className={styles['search-container']}>
-          <Link href="/">
-            <h1>soundTrack</h1>
-          </Link>
-          <form onSubmit={handleSearch}>
-            <input
-              type="text"
-              placeholder="Search..."
-              className={styles['search-input']}
-            />
-          </form>
-        </div>
-        {isAuthenticated && (
-          <div>
-            {user!.picture && (
-              <Image src={user!.picture} alt="" width="50" height="50"></Image>
-            )}
+    <div className={styles['navbar']}>
+      <Link href="/">
+        <h1>soundTrack</h1>
+      </Link>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search..."
+          className={styles['search-input']}
+        />
+      </form>
+      {user && (
+        <div>
+          <button className={styles['user-profile']}>
+            <Image
+              src={user.picture!}
+              alt="user profile"
+              width="45"
+              height="45"
+              className={styles['user-profile-img']}
+            ></Image>
+          </button>
 
-            <h2>{user!.username}</h2>
-            <p>{user!.email}</p>
-            <button
+          {/* <h2>{user!.username}</h2>
+            <p>{user!.email}</p> */}
+          {/* <button
               onClick={() =>
                 logout({
                   logoutParams: {
@@ -42,30 +45,29 @@ const NavBar = ({}) => {
               }
             >
               Log Out
-            </button>
-          </div>
-        )}
-        {!user && (
-          <div>
-            <button
-              className={`${styles['btn']} ${styles['btn-primary']}`}
-              onClick={() => loginWithRedirect()}
-            >
-              LOG IN
-            </button>
-            <button
-              className={`${styles['btn']} ${styles['btn-secondary']}`}
-              onClick={() =>
-                loginWithRedirect({
-                  authorizationParams: { screen_hint: 'signup' }
-                })
-              }
-            >
-              CREATE ACCOUNT
-            </button>
-          </div>
-        )}
-      </div>
+            </button> */}
+        </div>
+      )}
+      {!user && (
+        <div>
+          <button
+            className={`${styles['btn']} ${styles['btn-primary']}`}
+            onClick={() => loginWithRedirect()}
+          >
+            LOG IN
+          </button>
+          <button
+            className={`${styles['btn']} ${styles['btn-secondary']}`}
+            onClick={() =>
+              loginWithRedirect({
+                authorizationParams: { screen_hint: 'signup' }
+              })
+            }
+          >
+            CREATE ACCOUNT
+          </button>
+        </div>
+      )}
     </div>
   );
 };
