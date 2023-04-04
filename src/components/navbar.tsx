5a9b0fdb383dfe561ce18e2b9190e4fd653326bc
Daniel Chance <dan.chance@outlook.com>
2023-04-04 19:39:25 +0100

    add styles for login buttons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,49 +2,70 @@ import { useAuth0 } from '@auth0/auth0-react';
 import Image from 'next/image';
 import styles from '@/styles/components/navbar.module.sass';
 import { useEffect } from 'react';
+import Link from 'next/link';
 
 const NavBar = ({}) => {
   const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-  const getTrack = async () => {
-    const data = await fetch('http://localhost:8000/api/tracks/123');
-
-    // console.log(await data.json());
-  };
-
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {};
 
   return (
     <div className={styles['container']}>
-      {isAuthenticated && (
-        <div>
-          {user!.picture && (
-            <Image src={user!.picture} alt="" width="50" height="50"></Image>
-          )}
+      <div className={styles['navbar']}>
+        <div className={styles['search-container']}>
+          <Link href="/">
+            <h1>soundTrack</h1>
+          </Link>
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Search..."
+              className={styles['search-input']}
+            />
+          </form>
+        </div>
+        {isAuthenticated && (
+          <div>
+            {user!.picture && (
+              <Image src={user!.picture} alt="" width="50" height="50"></Image>
+            )}
 
-          <h2>{user!.username}</h2>
-          <p>{user!.email}</p>
-          <button
-            onClick={() =>
-              logout({
-                logoutParams: {
-                  returnTo: process.env.NEXT_PUBLIC_AUTH0_CALLBACK_URL
-                }
-              })
-            }
-          >
-            Log Out
-          </button>
-          <button onClick={getTrack}>Get Track</button>
-        </div>
-      )}
-      {!user && (
-        <div>
-          <button onClick={() => loginWithRedirect()}>Log In</button>
-        </div>
-      )}
+            <h2>{user!.username}</h2>
+            <p>{user!.email}</p>
+            <button
+              onClick={() =>
+                logout({
+                  logoutParams: {
+                    returnTo: process.env.NEXT_PUBLIC_AUTH0_CALLBACK_URL
+                  }
+                })
+              }
+            >
+              Log Out
+            </button>
+          </div>
+        )}
+        {!user && (
+          <div>
+            <button
+              className={`${styles['btn']} ${styles['btn-primary']}`}
+              onClick={() => loginWithRedirect()}
+            >
+              LOG IN
+            </button>
+            <button
+              className={`${styles['btn']} ${styles['btn-secondary']}`}
+              onClick={() =>
+                loginWithRedirect({
+                  authorizationParams: { screen_hint: 'signup' }
+                })
+              }
+            >
+              CREATE ACCOUNT
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
