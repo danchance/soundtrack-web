17f61b71607cd886059b4a8d2bee85d163ae2d0e
Daniel Chance <dan.chance@outlook.com>
2023-04-09 11:53:06 +0100

    fix navbar resizing on small screens

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,9 +3,14 @@ import Image from 'next/image';
 import styles from '@/styles/components/navbar.module.sass';
 import Link from 'next/link';
 import SearchIcon from '@/assets/icons/search.png';
+import { useRef } from 'react';
+import useWindowSize from '@/hooks/useWindowSize';
 
 const NavBar = ({}) => {
-  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const search = useRef<HTMLInputElement>(null);
+  const windowSize = useWindowSize();
+  const { isLoading, user, isAuthenticated, loginWithRedirect, logout } =
+    useAuth0();
 
   const handleSearch = () => {};
 
@@ -14,31 +19,52 @@ const NavBar = ({}) => {
       <Link href="/" className={styles['site-heading']}>
         <h1>soundTrack</h1>
       </Link>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          placeholder="Search..."
-          className={styles['search-input']}
-        />
-        <button>
-          <Image src={SearchIcon} alt="search" width="20" height="20"></Image>
-        </button>
-      </form>
-      {user && (
-        <div>
-          <button className={styles['user-profile']}>
-            <Image
-              src={user.picture!}
-              alt="user profile"
-              width="45"
-              height="45"
-              className={styles['user-profile-img']}
-            ></Image>
+      <div
+        className={`${styles['search-container']} ${
+          windowSize.width < 600 ? styles['btn-only'] : ''
+        }`}
+      >
+        {windowSize.width < 600 && (
+          <button className={styles['search-btn']}>
+            <Image src={SearchIcon} alt="search" width="20" height="20"></Image>
           </button>
+        )}
+        {windowSize.width >= 600 && (
+          <form onSubmit={handleSearch}>
+            <input
+              ref={search}
+              type="text"
+              placeholder="Search..."
+              className={styles['search-input']}
+            />
+            <button className={styles['search-btn']}>
+              <Image
+                src={SearchIcon}
+                alt="search"
+                width="20"
+                height="20"
+              ></Image>
+            </button>
+          </form>
+        )}
+      </div>
+      {!isLoading && (
+        <>
+          {user && (
+            <div>
+              <button className={styles['user-profile']}>
+                <Image
+                  src={user.picture!}
+                  alt="user profile"
+                  width="45"
+                  height="45"
+                  className={styles['user-profile-img']}
+                ></Image>
+              </button>
 
-          {/* <h2>{user!.username}</h2>
+              {/* <h2>{user!.username}</h2>
             <p>{user!.email}</p> */}
-          {/* <button
+              {/* <button
               onClick={() =>
                 logout({
                   logoutParams: {
@@ -49,27 +75,29 @@ const NavBar = ({}) => {
             >
               Log Out
             </button> */}
-        </div>
-      )}
-      {!user && (
-        <div>
-          <button
-            className={`${styles['btn']} ${styles['btn-primary']}`}
-            onClick={() => loginWithRedirect()}
-          >
-            LOG IN
-          </button>
-          <button
-            className={`${styles['btn']} ${styles['btn-secondary']}`}
-            onClick={() =>
-              loginWithRedirect({
-                authorizationParams: { screen_hint: 'signup' }
-              })
-            }
-          >
-            CREATE ACCOUNT
-          </button>
-        </div>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <button
+                className={`${styles['btn']} ${styles['btn-primary']}`}
+                onClick={() => loginWithRedirect()}
+              >
+                LOG IN
+              </button>
+              <button
+                className={`${styles['btn']} ${styles['btn-secondary']}`}
+                onClick={() =>
+                  loginWithRedirect({
+                    authorizationParams: { screen_hint: 'signup' }
+                  })
+                }
+              >
+                CREATE ACCOUNT
+              </button>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
