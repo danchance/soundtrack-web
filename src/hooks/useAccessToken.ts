78f77f23178821b0252127c893ea9915bb084068
Daniel Chance <dan.chance@outlook.com>
2023-04-20 19:34:57 +0100

    add hook to fetch the users access token

---
+++ src/hooks/useAccessToken.ts
@@ -0,0 +1,33 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useRef, useState } from 'react';
+
+/**
+ * Custom hook used to fetch the access token for the user.
+ * @returns The access token for the user.
+ */
+const useAccessToken = (): { accessToken: string } => {
+  const [accessToken, setAccessToken] = useState<string>('');
+  const { user, getAccessTokenSilently } = useAuth0();
+
+  /**
+   * Fetch the access token for the user.
+   */
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE
+          }
+        });
+        setAccessToken(token);
+      } catch (error) {
+        setAccessToken('');
+      }
+    })();
+  }, [user, getAccessTokenSilently]);
+
+  return { accessToken };
+};
+
+export default useAccessToken;
