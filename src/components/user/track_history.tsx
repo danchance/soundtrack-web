93a42d42f1f1bf38a84844490f453a3326452b16
Daniel Chance <dan.chance@outlook.com>
2023-05-04 19:17:17 +0100

    update track history to fetch data

---
+++ src/components/user/track_history.tsx
@@ -0,0 +1,76 @@
+import styles from '@/styles/components/user/track_history.module.sass';
+import { formatDate } from '@/utils/format_date_time';
+import Image from 'next/image';
+import Link from 'next/link';
+import { RecentlyPlayedTrack } from '@/utils/types';
+import useFetch from '@/hooks/useFetch';
+import LoadingSpinner from '../loading_spinner';
+
+type TrackHistoryProps = {
+  username: string;
+};
+
+type RecentlyPlayedResponse = {
+  recentTracks: RecentlyPlayedTrack[];
+};
+
+/**
+ * Component for displaying a users recently played tracks.
+ * @param itemList The list of tracks to display.
+ */
+const TrackHistory = ({ username }: TrackHistoryProps) => {
+  const url = `http://localhost:8000/api/users/${username}/track-history`;
+  const { isLoading, data } = useFetch<RecentlyPlayedResponse>(url);
+
+  return (
+    <div className={styles['container']}>
+      <div className={styles['header']}>
+        <h2>RECENTLY PLAYED</h2>
+      </div>
+      {isLoading && <LoadingSpinner height={5} />}
+      {data && (
+        <table className={styles['list']}>
+          <tbody>
+            {data.recentTracks.map((item) => (
+              <tr key={item.id}>
+                <td className={styles['artwork-col']}>
+                  <Link
+                    href={`/library/${item.Track.Album.Artist.slug}/${item.Track.Album.slug}/${item.Track.slug}`}
+                  >
+                    <Image
+                      className={styles['artwork']}
+                      src={item.Track.Album.artwork}
+                      alt={item.Track.name}
+                      width={40}
+                      height={40}
+                    ></Image>
+                  </Link>
+                </td>
+                <td className={styles['track-col']}>
+                  <Link
+                    href={`/library/${item.Track.Album.Artist.slug}/${item.Track.Album.slug}/${item.Track.slug}`}
+                  >
+                    {item.Track.name}
+                  </Link>
+                </td>
+                <td className={styles['artist-col']}>
+                  <Link href={`/library/${item.Track.Album.Artist.slug}`}>
+                    {item.Track.Album.Artist.name}
+                  </Link>
+                </td>
+                <td className={styles['time-col']}>
+                  <p>{formatDate(item.playedAt)}</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <div className={styles['footer']}>
+        <Link href="#">View All</Link>
+      </div>
+    </div>
+  );
+};
+
+export default TrackHistory;
--- src/pages/user/[user]/index.tsx
+++ src/pages/user/[user]/index.tsx
@@ -1,67 +1,25 @@
 import ProfileLayout from '@/layouts/profile_layout';
 import styles from '@/styles/pages/user/profile.module.sass';
 import { NextPageWithLayout } from '@/pages/_app';
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement } from 'react';
 import { UserPage } from '@/components/user/profile_nav';
 import { useRouter } from 'next/router';
-import RecentlyPlayed from '@/components/user/recently_played';
-import useFetch from '@/hooks/useFetch';
-import LoadingSpinner from '@/components/loading_spinner';
-import { RecentlyPlayedTrack } from '@/utils/types';
+import TrackHistory from '@/components/user/track_history';
 import TopItemDisplay, {
   TopItemTypes
 } from '@/components/user/top_items/top_item_display';
-
-type RecentlyPlayedResponse = {
-  recentTracks: RecentlyPlayedTrack[];
-};
 
 /**
  * User Profile page.
  */
 const Profile: NextPageWithLayout = () => {
   const user = useRouter().query.user;
-  const [url, setUrl] = useState<string>('');
-  const { isLoading, error, data } = useFetch<RecentlyPlayedResponse>(url);
-
-  /**
-   *
-   *
-   * TODO: Change request to recently played endpoint and remove profile endpoint.
-   *
-   *
-   *
-   */
-  useEffect(() => {
-    if (user !== undefined) {
-      setUrl(`http://localhost:8000/api/users/${user as string}/profile`);
-    }
-  }, [user]);
-
-  if (error) {
-    return (
-      <>
-        <div className={styles['error']}>
-          <p>Looks like something went wrong! :(</p>
-          <button>Try again</button>
-        </div>
-      </>
-    );
-  }
-
-  if (isLoading) {
-    return (
-      <div className={styles['spinner']}>
-        <LoadingSpinner height={5} />
-      </div>
-    );
-  }
 
   return (
     <div className={styles['container']}>
-      {data && <RecentlyPlayed itemList={data.recentTracks} />}
       {user && (
         <>
+          <TrackHistory username={user as string} />
           <TopItemDisplay
             username={user as string}
             itemType={TopItemTypes.TRACK}
