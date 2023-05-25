b8066052d1cb0f35425ee031990572181d36762d
Daniel Chance <dan.chance@outlook.com>
2023-05-25 16:26:25 +0100

    fix on small screens

--- src/components/page_number_nav.tsx
+++ src/components/page_number_nav.tsx
@@ -1,3 +1,4 @@
+import useWindowSize from '@/hooks/useWindowSize';
 import styles from '@/styles/components/page_number_nav.module.sass';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
@@ -20,31 +21,45 @@ const PageNumberNav = ({
   setCurrentPage
 }: PageNumberNavProps) => {
   const [pageNumbersList, setPageNumbersList] = useState<number[]>([]);
+  const { width } = useWindowSize();
 
   /**
    * Calculate the page numbers to display, want to display the current page and the
-   * 2 previous and next pages, e.g. for page 4: 2 3 (4) 5 6.
+   * 1/2 previous and next pages, e.g. for page 4: 2 3 (4) 5 6.
    * Special cases:
    *  - Less than 5 pages, display all page numbers
    *  - 2 previous pages do not exist, display more next pages, e.g. 1 (2) 3 4 5
    *  - 2 next pages do not exist, display more previous pages, e.g. 3 4 5 6 (7)
+   *  - Screen width < 500px, only display 1 previous and next page
    */
   useEffect(() => {
     const pageNumbers = [...Array(maxPages).keys()].map((i) => i + 1);
-    if (maxPages <= 5) {
+    // Calculate items displayed based on screen size. Left offset is larger as it
+    // includes the current page.
+    let displayedNumbers = 5;
+    let leftOffset = 3;
+    let rightOffset = 2;
+    if (width < 500) {
+      displayedNumbers = 3;
+      leftOffset = 2;
+      rightOffset = 1;
+    }
+    if (maxPages <= displayedNumbers) {
       setPageNumbersList(pageNumbers);
       return;
     }
-    if (currentPage - 3 < 0) {
-      setPageNumbersList(pageNumbers.slice(0, 5));
+    if (currentPage - leftOffset < 0) {
+      setPageNumbersList(pageNumbers.slice(0, displayedNumbers));
       return;
     }
-    if (currentPage + 2 > maxPages) {
-      setPageNumbersList(pageNumbers.slice(-5));
+    if (currentPage + rightOffset > maxPages) {
+      setPageNumbersList(pageNumbers.slice(-displayedNumbers));
       return;
     }
-    setPageNumbersList(pageNumbers.slice(currentPage - 3, currentPage + 2));
-  }, [currentPage, maxPages]);
+    setPageNumbersList(
+      pageNumbers.slice(currentPage - leftOffset, currentPage + rightOffset)
+    );
+  }, [currentPage, maxPages, width]);
 
   return (
     <div className={styles['container']}>
