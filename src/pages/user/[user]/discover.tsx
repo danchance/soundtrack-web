cd284ab52355fe19666ea7058fcaebb86110cc11
Daniel Chance <dan.chance@outlook.com>
2023-06-14 18:58:47 +0100

    add coming soon info

--- src/pages/user/[user]/discover.tsx
+++ src/pages/user/[user]/discover.tsx
@@ -2,14 +2,22 @@ import { UserPage } from '@/components/user/profile_nav';
 import ProfileLayout from '@/layouts/profile_layout';
 import styles from '@/styles/pages/user/discover.module.sass';
 import { ReactElement } from 'react';
+import TempRobotImage from '@/assets/images/404.svg';
+import Image from 'next/image';
 
 /**
  * User Discover page.
  */
 const Discover = () => {
   return (
-    <div className={styles['discover']}>
-      <h2>Discover</h2>
+    <div className={styles['container']}>
+      <h2>COMING SOON...</h2>
+      <Image
+        src={TempRobotImage}
+        alt="coming soon robot"
+        width={200}
+        height={200}
+      ></Image>
     </div>
   );
 };
--- src/pages/user/[user]/recap.tsx
+++ src/pages/user/[user]/recap.tsx
@@ -2,14 +2,22 @@ import { UserPage } from '@/components/user/profile_nav';
 import ProfileLayout from '@/layouts/profile_layout';
 import styles from '@/styles/pages/user/recap.module.sass';
 import { ReactElement } from 'react';
+import TempRobotImage from '@/assets/images/404.svg';
+import Image from 'next/image';
 
 /**
  * User Recap page.
  */
 const Recap = () => {
   return (
-    <div className={styles['recap']}>
-      <h2>Recap</h2>
+    <div className={styles['container']}>
+      <h2>COMING SOON...</h2>
+      <Image
+        src={TempRobotImage}
+        alt="coming soon robot"
+        width={200}
+        height={200}
+      ></Image>
     </div>
   );
 };
