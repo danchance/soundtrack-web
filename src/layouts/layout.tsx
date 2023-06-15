c64c6ab328926c5a3154e5e75017e5fa7b9dbf5d
Daniel Chance <dan.chance@outlook.com>
2023-06-15 21:55:53 +0100

    update favicon

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -26,7 +26,24 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           content="SoundTrack, streaming history tracker."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
       </Head>
       <div className={styles['container']}>
         <NavBar hasBackground={hasBackground} />
